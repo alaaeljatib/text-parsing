61486baa0a0946c65d26527244bf72ae46656a59
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-03-27 16:56:20 -0400

    Add comments

--- Text Parsing/Controller/TextParsingViewController.swift
+++ Text Parsing/Controller/TextParsingViewController.swift
@@ -11,31 +11,38 @@ import UIKit
 class TextParsingViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     
-    
+    // get the word dictionary from Data Service
     let wordsDict = DataService.instance.provideWordsDictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Assign the delegate to let the logic in the extention below work
         self.textView.delegate = self
         
+        // add gesture to dismiss keyboard when tapping outside the text area
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
     }
     
     @IBAction func parseButtonPressed(_ sender: Any) {
+        // hide keyboard
         self.textView.resignFirstResponder()
         
+        // make sure there is text to parse
         guard self.textView.text.count > 0 else {
             return
         }
         
+        // stop input restriction mode
         self.textView.delegate = nil
-        self.textView.text = ParseService.instance.parse(content: self.textView.text.lowercased(), wordsDict: self.wordsDict)
         
+        // call the logic
+        self.textView.text = ParseService.instance.parse(content: self.textView.text.lowercased(), wordsDict: self.wordsDict)
     }
     
     @IBAction func startOverPressed(_ sender: Any) {
+        // hide the keyboard and reset the place holder
         self.textView.resignFirstResponder()
         textView.textColor = UIColor.lightGray
         textView.text = "Enter your text.."
@@ -43,12 +50,14 @@ class TextParsingViewController: UIViewController {
     }
     
     @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        // hide the keyboard
         self.textView.resignFirstResponder()
     }
 }
 
 extension TextParsingViewController : UITextViewDelegate {
     
+    // make sure only letters allowed as input
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if String(text.unicodeScalars.filter{CharacterSet.decimalDigits.contains($0) || $0 == " "}).count > 0 {
             return false
@@ -56,6 +65,7 @@ extension TextParsingViewController : UITextViewDelegate {
         return true
     }
     
+    // handling the place holder when start editing
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.text == "Enter your text.." {
             textView.text = ""
@@ -64,6 +74,7 @@ extension TextParsingViewController : UITextViewDelegate {
         
     }
     
+    // handling the place holder when end editing
     func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.count == 0 {
             textView.textColor = UIColor.lightGray
