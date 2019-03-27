23a558be5d5905543b5413eb39daad2c0016b516
Alaa Eljatib Etmaz Alsebaei <alaa@alaas-new-macbook-pro.local>
2019-03-27 11:30:11 -0400

    Initial Commit

---
+++ Text Parsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Parsing
+//
+//  Created by Alaa Eljatib Etmaz Alsebaei on 2019-03-27.
+//  Copyright © 2019 Alaa Eljatib Etmaz Alsebaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
